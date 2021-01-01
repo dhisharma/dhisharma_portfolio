9dfac432bfe64607f8a3f2e22e7bf93b0e4ec6a8
V-Sharma10 <2017ugcs071@nitjsr.ac.in>
2021-01-01 13:46:28 +0530

    1st layout

---
+++ src/components/layouts/menus.js
@@ -0,0 +1,27 @@
+const menus = [
+    {
+        id: 1,
+        tomenu: "#about",
+        namemenu: "About"
+    },
+    {
+        id: 2,
+        tomenu: "#services",
+        namemenu: "Services"
+    },
+    {
+        id: 3,
+        tomenu: "#portfolio",
+        namemenu: "Portfolio"
+    },
+    {
+        id: 4,
+        tomenu: "#testimonial",
+        namemenu: "Testimonial"
+    },
+    
+];
+
+
+
+export default menus ;
