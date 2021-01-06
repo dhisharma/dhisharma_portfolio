b9ac2d9b205ebbf8479221399ecc3d4f395d778f
V-Sharma10 <2017ugcs071@nitjsr.ac.in>
2021-01-06 13:52:52 +0530

    projects added

--- src/components/layouts/home04/CaseStudy.jsx
+++ src/components/layouts/home04/CaseStudy.jsx
@@ -9,20 +9,24 @@ class CaseStudy extends Component {
                         <div className="col-left animate-element wow delay5 fadeInDown" data-wow-delay="0.5s">
                             <div className="image-link t2">
                                 <div className="featured-post">
-                                    <Link to="#"><img src="images/section/52.jpg" alt="images" /></Link>
+                                    <Link to="#"><img src="images/Projects/medre.png" alt="images" /></Link>
+                                    
+                                    <Link to="#" className="text-white  ">Medre</Link>
                                 </div>
                                 <div className="portfolio-template-link text-center">
-                                    <Link to="#" className="text-white">Portfolio Template</Link>
+                                    {/* <Link to="#" className="text-white">Medre</Link> */}
                                 </div>
                             </div>
                             <div className="image-link t2">
                                 <div className="featured-post">
-                                    <Link to="#"><img src="images/section/53.jpg" alt="images" /></Link>
+                                    <Link to="#"><img src="images/Projects/calheers.png" alt="images" /></Link>
+                                    
+                                    <Link to="#" className="text-white">Calheers</Link>
                                 </div>
                             </div>
-                            <div className="fl-btn text-center"> 
+                            {/* <div className="fl-btn text-center"> 
                                 <Link to="#" className="f-w500">Explore More</Link>
-                            </div>
+                            </div> */}
                         </div>
                         <div className="flat-spacer" data-desktop={0} data-mobile={100} data-smobile={100} />
                             <div className="col-right animate-element wow delay5 fadeInUp" data-wow-delay="0.5s">
@@ -36,12 +40,15 @@ class CaseStudy extends Component {
                             <div className="pd-right">
                                 <div className="image-link t2">
                                     <div className="featured-post">
-                                        <Link to="#"><img src="images/section/54.jpg" alt="images" /></Link>
+                                        <Link to="#"><img src="images/Projects/sams.png" alt="images" /></Link>
+                                        
+                                    <Link to="#" className="text-white">Sams</Link>
                                     </div>
                                 </div>
                                 <div className="image-link t2">
                                     <div className="featured-post">
-                                        <Link to="#"><img src="images/section/55.jpg" alt="images" /></Link>
+                                        <Link to="#"><img src="images/Projects/travel-site.png" alt="images" /></Link>
+                                        <Link to="#" className="text-white">Travel Site</Link>
                                     </div>
                                 </div>
                             </div>
