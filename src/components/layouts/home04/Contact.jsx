30dedb0ced1c3baf65e7a2def3662b1e7d0de5b4
Abhishek Kumar <abhishekkumarjnvk@gmail.com>
2021-03-22 15:39:00 +0530

    Mailing Feature

--- src/components/layouts/home04/Contact.jsx
+++ src/components/layouts/home04/Contact.jsx
@@ -1,104 +1,132 @@
-import React, { useState } from 'react';
-import { Mailer } from 'nodemailer-react';
+import React, { useState } from "react";
+import emailjs from "emailjs-com";
+import swal from "sweetalert";
 
 const Contact = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
   const [phone, setPhone] = useState(0);
-  const [subject, setSubject] = useState('');
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
 
-  const sendMail = () => {
+  const sendMail = (e) => {
+    e.preventDefault();
     console.log({ name, email, phone, subject });
-    let mailerConfig = {
-      host: 'smtp.example.com',
-      port: 587,
-      secure: false, // upgrade later with STARTTLS
-      auth: {
-        user: 'username',
-        pass: 'password',
-      },
-    };
-    const mailer = Mailer(mailerConfig, ['']);
+    if (name && email && phone && subject && message) {
+      const templateParams = {
+        from_name: name,
+        reply_to: email,
+        email:email,
+        message: message,
+        subject: subject,
+        phone: phone,
+      };
+      emailjs
+        .send(
+          "service_q2jc01m",
+          "template_nxdkyr9",
+          templateParams,
+          "user_L8GMgfUjSDd6weAIpR0qU"
+        )
+        .then(
+          (response) => {
+            swal("Message Sent!", "", "success");
+            console.log("SUCCESS!", response.status, response.text);
+          },
+          (err) => {
+            console.log("FAILED...", err);
+            swal("Failed!", "", "error");
+          }
+        );
+    } else {
+      swal("Error!", "Missing Required Field", "error");
+    }
   };
   return (
-    <section className='contact' id='contact'>
-      {/* email
-      name
-      phone
-      subject */}
-      <h3 className='contact-intro  text-center'>Contact me!</h3>
-      <div className='form-div text-center'>
-        <form className='form'>
-          <div className='form-group display-flex row'>
-            <label for='email' className='form-label col-md-4'>
-              Email
+    <section className="contact" id="contact">
+      <h3 className="contact-intro  text-center">Contact me!</h3>
+      <div className="form-div text-center">
+        <form className="form" onSubmit={sendMail}>
+          <div className="form-group display-flex row">
+            <label for="email" className="form-label col-md-4">
+              {/* Email */}
             </label>
             <input
-              type='email'
-              className='form-control fields'
-              placeholder='Email'
-              className='col-md-8'
-              id='email'
+              type="email"
+              className="form-control fields col-md-8"
+              placeholder="Email"
+              id="email"
               onChange={(e) => {
                 setEmail(e.target.value);
-                console.log('email ' + email);
+                console.log("email " + email);
               }}
             />
           </div>
-          <div className='form-group row'>
-            <label for='name' className='form-label col-md-4'>
-              Name
+          <div className="form-group row">
+            <label for="name" className="form-label col-md-4">
+              {/* Name */}
             </label>
 
             <input
-              type='text'
-              className='form-control fields'
-              placeholder='Name'
-              className='col-md-8'
-              id='name'
+              type="text"
+              className="form-control fields col-md-8"
+              placeholder="Name"
+              id="name"
               onChange={(e) => setName(e.target.value)}
             />
           </div>
-          <div className='form-group row'>
-            <label for='phone' className='form-label col-md-4'>
-              Phone
+          <div className="form-group row">
+            <label for="phone" className="form-label col-md-4">
+              {/* Phone */}
             </label>
 
             <input
-              type='number'
-              className='form-control fields'
-              placeholder='Phone Number'
-              className='col-md-8'
-              id='phone'
+              type="number"
+              className="form-control fields col-md-8"
+              placeholder="Phone Number"
+              id="phone"
               onChange={(e) => setPhone(e.target.value)}
             />
           </div>
 
-          <div className='form-group row'>
+          <div className="form-group row">
             <label
-              for='subject'
-              placeholder='Subject'
-              className='form-label col-md-4'
+              for="subject"
+              placeholder="Subject"
+              className="form-label col-md-4"
             >
-              Subject
+              {/* Subject */}
+            </label>
+
+            <input
+              id="subject"
+              className="form-control fields col-md-8"
+              row="10"
+              type="text"
+              onChange={(e) => setSubject(e.target.value)}
+            />
+          </div>
+          <div className="form-group row">
+            <label
+              for="subject"
+              placeholder="Subject"
+              className="form-label col-md-4"
+            >
+              {/* Message */}
             </label>
 
             <textarea
-              id='subject'
-              class='form-control col-md-8'
-              row='10'
-              onChange={(e) => setSubject(e.target.value)}
+              id="subject"
+              class="form-control col-md-8"
+              row="10"
+              onChange={(e) => setMessage(e.target.value)}
             ></textarea>
           </div>
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              sendMail(name, email, phone, subject);
-            }}
-          >
-            {' '}
-            Submit{' '}
-          </button>
+          <center>
+            <button>
+              Submit
+            </button>
+          </center>
         </form>
       </div>
     </section>
